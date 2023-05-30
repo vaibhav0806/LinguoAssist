b807e13199dd8b8390c25375dd4cc717c45fe278
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2023-05-30 22:19:33 +0530

    Update catch errors

--- background.js
+++ background.js
@@ -5,9 +5,14 @@ async function logJSONData(str) {
   const response = await fetch(
     `https://api.dictionaryapi.dev/api/v2/entries/en/${str}`
   );
-  const jsonData = await response.json();
-  const def = jsonData[0].meanings[0].definitions[0].definition;
-  return def;
+  console.log(response.status);
+  if (response.status === 200) {
+    const jsonData = await response.json();
+    const def = jsonData[0].meanings[0].definitions[0].definition;
+    return def;
+  } else {
+    return null;
+  }
 }
 
 chrome.runtime.onInstalled.addListener(function () {
@@ -22,11 +27,19 @@ chrome.contextMenus.onClicked.addListener(async function (info, tab) {
   if (info.menuItemId === "defineContextMenu") {
     selectedWord = info.selectionText;
     def = await logJSONData(selectedWord);
+    if (!def) {
+      selectedWord = "No Definitions Found";
+      def =
+        "Sorry pal, we couldn't find definitions for the word you were looking for.";
+    } else {
+      selectedWord =
+        selectedWord.charAt(0).toUpperCase() + selectedWord.substring(1);
+    }
     chrome.notifications.create(
       {
         type: "basic",
         iconUrl: "icons/icon-32.png",
-        title: selectedWord.charAt(0).toUpperCase() + selectedWord.substring(1),
+        title: selectedWord,
         message: def,
         requireInteraction: true,
         buttons: [
@@ -51,6 +64,10 @@ chrome.notifications.onButtonClicked.addListener(
       chrome.tabs.create({
         url: `https://www.google.com/search?q=define+${selectedWord}`,
       });
+    } else if (buttonIndex === 1) {
+      chrome.notifications.clear(notificationId, () =>
+        console.log("CLosed notification", notificationId)
+      );
     }
   }
 );
